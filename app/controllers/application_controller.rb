bf92d78468c5eee0e5c247757f77243447fbe2df
west03 <zxi6557@gmail.com>
2021-10-26 08:40:31 +0000

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
   def confingure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: %i[name last_name first_name last_name_kana first_name_kana postal_code
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_120027) do
+ActiveRecord::Schema.define(version: 2021_10_25_212626) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,36 @@ ActiveRecord::Schema.define(version: 2021_10_24_120027) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "guest_id"
+    t.integer "event_id"
+    t.string "comment_content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.integer "guest_id"
+    t.integer "host_id"
+    t.integer "genre_id"
+    t.string "title"
+    t.text "introduction"
+    t.string "image_id"
+    t.string "participant"
+    t.integer "prefecture"
+    t.string "address"
+    t.string "date_and_time"
+    t.integer "holding_flag", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "guests", force: :cascade do |t|
@@ -59,6 +89,26 @@ ActiveRecord::Schema.define(version: 2021_10_24_120027) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_hosts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_hosts_on_reset_password_token", unique: true
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "guest_id"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tag_maps", force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
