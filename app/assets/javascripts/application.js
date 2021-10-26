b08aacd95a303e24dc4deda55b3645dd467445c1
west03 <zxi6557@gmail.com>
2021-10-26 12:58:00 +0000

    add devise

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 
-//=require jquery3
-//=require popper
-//=require bootstrap-sprickets
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 
 //= require rails-ujs
 //= require activestorage
