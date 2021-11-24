66b19e3ab913f5447a76b83973ec5014917906d7
west03 <zxi6557@gmail.com>
2021-11-24 09:13:14 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 Admin.create(email: "admin@com", password: "119216")
